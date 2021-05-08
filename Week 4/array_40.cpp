f62b9e9af2f07b94534d6900250daf77f1cc036e
shashwat286 <63719276+shashwat286@users.noreply.github.com>
2021-05-08 17:53:59 +0530

    Add files via upload

---
+++ Week 4/array_40.cpp
@@ -0,0 +1,7 @@
+void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k%n);
+        reverse(nums.begin()+k%n,nums.end());
+        
+    }
